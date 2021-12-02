ac32117dc5bb1c109f1e18b25b0ebd067e0de697
Alexandre Sampaio <thndrmaster@gmail.com>
2021-12-02 11:58:30 -0300

    Criando CRUD com Class Based Views e Custom CSS

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import IndexView, CreateProdutoView, UpdateProdutoView,DeleteProdutoView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('produtos/', IndexView.as_view(), name='produtos')
+    path('add-produto/', CreateProdutoView.as_view(), name='add-produto'),
+    path('produto/<int:pk>/update', UpdateProdutoView.as_view(), name='update-produto'),
+    path('produto/<int:pk>/delete', DeleteProdutoView.as_view(), name='delete-produto'),
+
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,7 @@
+from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-
 from .models import Produto
-from .forms import ProdutoModelForm
 
 
 class IndexView(ListView):
@@ -10,3 +9,23 @@ class IndexView(ListView):
     template_name = 'index.html'
     queryset = Produto.objects.all()
     context_object_name = 'produtos'
+
+
+class CreateProdutoView(CreateView):
+    model = Produto
+    template_name = 'produto.form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+
+class UpdateProdutoView(UpdateView):
+    model = Produto
+    template_name = 'produto.form.html'
+    fields = ['nome', 'preco']
+    success_url = reverse_lazy('index')
+
+
+class DeleteProdutoView(DeleteView):
+    model = Produto
+    template_name = 'produto.delete.html'
+    success_url = reverse_lazy('index')
