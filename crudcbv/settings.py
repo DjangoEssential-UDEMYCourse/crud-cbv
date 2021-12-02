4f364b7282c22fa154c3313f12f999fb32012ab0
Alexandre Sampaio <thndrmaster@gmail.com>
2021-12-02 16:47:00 -0300

    Customizando Template Admin LTE-3

--- crudcbv/settings.py
+++ crudcbv/settings.py
@@ -27,10 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-
 # Application definition
 
 INSTALLED_APPS = [
+    'adminlte3',
+    'adminlte3_theme',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -129,3 +130,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGOUT_REDIRECT_URL = 'index'
--- crudcbv/urls.py
+++ crudcbv/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('core.urls'))
 ]
+
+admin.AdminSite.site_header = "Sistema Delta"
+admin.AdminSite.site_title = "Curso da Geek University"
+admin.AdminSite.index_title = "Meu Dashboard Legal"
